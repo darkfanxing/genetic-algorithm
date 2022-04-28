4bebe24993206c8b100b7fa2cf3991e06c4f794e
darkfanxing <darkfanxing@gmail.com>
2022-04-28 15:03:17 +0800

    Modify function 2

--- src/functions/function_set/function_2.py
+++ src/functions/function_set/function_2.py
@@ -2,7 +2,7 @@ from numpy import power, sum
 
 def function_2(x, dimension=30):
     result = 0
-    for _ in range(dimension):
-        result += power(sum(x), 2)
+    for index in range(dimension):
+        result += power(sum(x[:index]), 2)
     
     return result
